d0f59cf3f82b37c11c3ac2fef76ca3b6000f8c34
Weijiang Yang <weiyang@f4r12s16.clusters.42paris.fr>
2026-03-17 15:57:20 +0100

    corrige tostring

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,17 +1,19 @@
 #include "Zombie.hpp"
+#include <sstream>
 
 int main()
 {
-    int i = 0;
-    while (i < 10)
-    {
-        std::string name;
-        
-        
-        name = "Zombie" + std::to_string(i);
-        randomChump(name);
-        
-        i++;
-    }
-    return (0);
+	int i = 0;
+	while (i < 10)
+	{
+		std::string name;
+
+		std::stringstream ss;
+		ss << i;
+		name = "Zombie" + ss.str();
+		randomChump(name);
+
+		i++;
+	}
+	return (0);
 }
