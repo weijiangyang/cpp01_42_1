94b71ffebc512cbc4ab907ae0249479d577f475b
Weijiang Yang <weiyang@f4r13s16.clusters.42paris.fr>
2026-03-19 12:05:06 +0100

    ex06

--- ex06/Harl.cpp
+++ ex06/Harl.cpp
@@ -1,31 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: weiyang <weiyang@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/19 12:03:05 by weiyang           #+#    #+#             */
+/*   Updated: 2026/03/19 12:03:06 by weiyang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Harl.hpp"
 
-Harl::Harl()
+Harl::Harl(void)
 {
 	std::cout << "harl is created." << std::endl;
 }
 
-Harl::~Harl()
+Harl::~Harl(void)
 {
 	std::cout << "harl is destroyed." << std::endl;
 }
 
-void Harl::debug(void) const
+void Harl::_debug(void) const
 {
 	std::cout << "DEBUG: I love having extra bacon for my burger!" << std::endl;
 }
 
-void Harl::info(void) const
+void Harl::_info(void) const
 {
 	std::cout << "INFO: I cannot believe adding extra bacon costs more money!" << std::endl;
 }
 
-void Harl::warning(void) const
+void Harl::_warning(void) const
 {
 	std::cout << "WARNING: I think I deserve extra bacon for free!" << std::endl;
 }
 
-void Harl::error(void) const
+void Harl::_error(void) const
 {
 	std::cout << "ERROR: This is unacceptable! I want to speak to the manager now." << std::endl;
 }
@@ -45,16 +57,16 @@ void Harl::complain(std::string level) const
 	{
 		case 0:
 			std::cout << "[ DEBUG ]" << std::endl;
-			this->debug();
+			this->_debug();
 		case 1:
 			std::cout << "[ INFO ]" << std::endl;
-			this->info();
+			this->_info();
 		case 2:
 			std::cout << "[ WARNING ]" << std::endl;
-			this->warning();
+			this->_warning();
 		case 3:
 			std::cout << "[ ERROR ]" << std::endl;
-			this->error();
+			this->_error();
 			break;
 		default:
 			std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
