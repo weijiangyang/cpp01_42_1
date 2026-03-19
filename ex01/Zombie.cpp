b0d2a539763340c083d290d8e02d6d26ae0b3ad8
Weijiang Yang <weiyang@f4r13s16.clusters.42paris.fr>
2026-03-19 10:31:15 +0100

    ex01

--- ex01/Zombie.cpp
+++ ex01/Zombie.cpp
@@ -1,34 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: weiyang <weiyang@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/19 10:30:20 by weiyang           #+#    #+#             */
+/*   Updated: 2026/03/19 10:30:21 by weiyang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
 #include "Zombie.hpp"
 
 Zombie::Zombie(void)
 {
-	//this->name = "default name";
+	this->_name = "default name";
 }
 
-Zombie::Zombie(std::string name):name(name)
+Zombie::Zombie(std::string name):_name(name)
 {
-    //this->name = name;
+    //this->_name = name;
 }
 
 Zombie::~Zombie()
 {
-    std::cout << this->name << " is destroyed!" << std:: endl;
+    std::cout << this->_name << " is destroyed!" << std:: endl;
 }
 
 void Zombie::setName(std::string name)
 {
-	this->name = name;
+	this->_name = name;
 }
 
 std::string Zombie::getName(void) const
 {
-	return this->name;
+	return this->_name;
 }
 
 
 void Zombie :: announce() const
 {
-    std::cout << this->name <<  " : BraiiiiiiinnnzzzZ..." << std::endl;
+    std::cout << this->_name <<  " : BraiiiiiiinnnzzzZ..." << std::endl;
 }
 
--- ex01/Zombie.hpp
+++ ex01/Zombie.hpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: weiyang <weiyang@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/19 10:26:55 by weiyang           #+#    #+#             */
+/*   Updated: 2026/03/19 10:27:02 by weiyang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef ZOMBIE_HPP
 # define ZOMBIE_HPP 
 
@@ -6,7 +18,7 @@
 class Zombie
 {
     private:
-        std::string name;
+        std::string _name;
     
     public:
 	    Zombie(void);
