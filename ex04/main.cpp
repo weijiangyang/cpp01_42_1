992c7b964acad6b42ac2222b8c9cec97a650a8f0
Weijiang Yang <weiyang@f4r13s16.clusters.42paris.fr>
2026-03-19 13:23:08 +0100

    ex04

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,13 +6,48 @@
 /*   By: weiyang <weiyang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/19 11:50:08 by weiyang           #+#    #+#             */
-/*   Updated: 2026/03/19 11:50:09 by weiyang          ###   ########.fr       */
+/*   Updated: 2026/03/19 13:17:29 by weiyang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <cerrno>
+#include <cstring>
+
+bool replaceFile(const std::string& fileName, const std::string& s1, const std::string& s2)
+{
+	std::ifstream infile(fileName.c_str());
+	if(!infile)
+	{
+		std::cerr << "Error: cannot open file " << fileName << ": " 
+		<< std::strerror(errno) << std::endl;
+		return (false);
+	}
+	std::ofstream outfile((fileName + ".replace").c_str());
+		if (!outfile)
+	{
+		std::cerr << "Error: cannot create output file" << fileName << ".replace:"
+		<< std::strerror(errno) << std::endl;
+        return (false);
+	}
+	std::string line;
+	while (std::getline(infile, line))
+	{
+		size_t pos = 0;
+		while ((pos = line.find(s1, pos)) != std::string::npos)
+		{
+			line.erase(pos, s1.length());
+			line.insert(pos, s2);
+			pos += s2.length();
+		}
+		outfile << line;
+		if(!infile.eof())
+			outfile << std::endl;
+	}
+	return (true);
+}
 
 int main(int argc, char **argv)
 {
@@ -31,33 +66,7 @@ int main(int argc, char **argv)
 		std::cerr << "Error: s1 cannot be empty" << std::endl;
 		return (1);
 	}
-	std::ifstream infile(filename.c_str());
-	if (!infile)
-	{
-		std::cerr << "Error: cannot open file" << std::endl;
-        return (1);
-	}
-	if (infile.peek() == std::ifstream::traits_type::eof())
-	{
-		std::cerr << "Error: file is empty" << std::endl;
+	if(!replaceFile(filename, s1, s2))
 		return (1);
-	}
-	std::ofstream outfile((filename + ".replace").c_str());
-	if (!outfile)
-	{
-		std::cerr << "Error: cannot create output file" << std::endl;
-        return (1);
-	}
-	while(std::getline(infile,line))
-	{
-		size_t pos = 0;
-		while ((pos = line.find(s1, pos)) != std::string::npos)
-		{
-			line.erase(pos, s1.length());
-			line.insert(pos, s2);
-			pos += s2.length();
-		}
-		outfile << line << std::endl;
-	}
 	return(0);
 }
