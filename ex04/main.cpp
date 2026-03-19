265432e23c8ec7a7697d1b1f3fe9d02d2d24cc53
Weijiang Yang <weiyang@f4r13s16.clusters.42paris.fr>
2026-03-19 11:53:06 +0100

    ex04

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: weiyang <weiyang@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/19 11:50:08 by weiyang           #+#    #+#             */
+/*   Updated: 2026/03/19 11:50:09 by weiyang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -12,37 +24,39 @@ int main(int argc, char **argv)
 	std::string filename = argv[1];
 	std::string s1 = argv[2];
 	std::string s2 = argv[3];
+	std::string line;
 
 	if (s1.empty())
 	{
-		std::cout << "Error: s1 cannot be empty" << std::endl;
+		std::cerr << "Error: s1 cannot be empty" << std::endl;
 		return (1);
 	}
 	std::ifstream infile(filename.c_str());
 	if (!infile)
 	{
-		std::cout << "Error: cannot open file" << std::endl;
-        return 1;
+		std::cerr << "Error: cannot open file" << std::endl;
+        return (1);
+	}
+	if (infile.peek() == std::ifstream::traits_type::eof())
+	{
+		std::cerr << "Error: file is empty" << std::endl;
+		return (1);
 	}
 	std::ofstream outfile((filename + ".replace").c_str());
 	if (!outfile)
 	{
-		std::cout << "Error: cannot create output file" << std::endl;
-        return 1;
+		std::cerr << "Error: cannot create output file" << std::endl;
+        return (1);
 	}
-	std::string line;
 	while(std::getline(infile,line))
 	{
-		size_t pos;
-
-		pos = 0;
+		size_t pos = 0;
 		while ((pos = line.find(s1, pos)) != std::string::npos)
 		{
 			line.erase(pos, s1.length());
 			line.insert(pos, s2);
-			pos+= s2.length();
+			pos += s2.length();
 		}
-		outfile << line;
 		outfile << line << std::endl;
 	}
 	return(0);
